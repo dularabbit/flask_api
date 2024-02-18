33e2159a6d96a41fec4c15e4fb2ea1bf445eff2f
dularabbit <90035293+dularabbit@users.noreply.github.com>
2024-02-18 10:13:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_index
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, BU'
+def index():
+    return render_index('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port="8000")
